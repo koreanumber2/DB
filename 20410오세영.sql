e4a67a157018ce5b8652eb4881176b200ae8fda8
koreanumber2 <ggi278837@gmail.com>
2025-11-18 10:00:14 +0900

    11-18

--- 20410오세영.sql
+++ 20410오세영.sql
@@ -1,54 +1,18 @@
-select * from dept7;
+create table book(book_id number(5)not null,category varchar2(10) not null,author_id number(5) not null,a number(10) not null,b date not null);
+drop table book;
+select * from book;
+insert into book values(1,'dlsans',1,10000,'2020-01-01');
+insert into book values(2,'rudwp',1,9000,'2021-04-11');
+insert into book values(3,'dlsans',2,11000,'2021-02-05');
+insert into book values(2,'rudwp',1,9000,'2021-04-11');
+insert into book values(2,'rudwp',1,9000,'2021-04-11');
 
-create table dept7
-as
-select * from dept2;
---±¸Á¶º¸±â
-desc dept7;
-alter table dept7 add(location varchar2(10));
+create table author(author_id number(5) not null,author_name varchar2(10) not null);
+insert into author values(1,'È«±æµ¿');
+insert into author values(1,'È«±æµ¿');insert into author values(1,'È«±æµ¿');insert into author values(1,'È«±æµ¿');
+select * from author;
 
-alter table dept7 rename column location to loc;
-
-rename table dept7 to dept6;
-
-alter table dept7 drop column location cascade constraints;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      ??
+    ¢Ñ¢Ð
+    ¢Ñ¢Ð
+?????????????????????¦®    
